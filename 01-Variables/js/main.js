912ec111c3f68015cf4827475d1a922bb640f82d
sarahleejara <sarahlee.jara@gmail.com>
2018-01-18 16:05:56 +0100

    add

---
+++ 01-Variables/js/main.js
@@ -0,0 +1,17 @@
+var enfants = Number(2);
+var femme = "Anna";
+var pays = "Bahamas";
+var metier = "blogger";
+
+console.log("Vous serez " + metier + " habiterez à " + pays + ", et marié a " + femme + " avec " + enfants + " enfants.")
+
+var année = Number(prompt("Entrer l'année courrante"));
+var naissance = Number(prompt("Entrer votre année de naissance"));
+var age = Number(année - naissance)
+console.log("Vous avez " + age + " ans.");
+
+var result = (1+2) * 3 + 4 / 2;
+var result = 3 + 6 + 2;
+var result = 9 + 2;
+var result = 11;
+console.log (result);
