e449528142aa7421699f4c1e7e691f7eb9ffa65b
Sarah Lee Jara <sarahlee.jara@gmail.com>
2018-02-28 12:44:03 +0100

    add

---
+++ 10-DOM/app.js
@@ -0,0 +1,9 @@
+let Body = document.querySelector("body");
+let premiere = document.getElementById('first-paragraph');
+// let silver = document.getElementByClassName('bg-silver');
+Body.classList.remove('bg-aqua');
+Body.classList.add('bg-olive');
+premiere.classList.remove("bg-lime", "gray");
+premiere.classList.add("aqua");
+// silver.classList.add('bg-teal');
+// silver.classList.remove('bg-silver');
